ca498facb1e497e01039a9fd4f9141053ab90bbd
Anvin.C.Shaju <anvincsg@gmail.com>
2023-12-30 07:00:11 +0530

    minor change

--- src/components/shared/Topbar.tsx
+++ src/components/shared/Topbar.tsx
@@ -46,7 +46,7 @@ const TopBar = () => {
             className="flex-center gap-3"
           >
             <img
-              src={user.imageUrl || "/assets/images/profile-placeholder.svg"}
+              src={user.imageUrl || "/assets/icons/profile-placeholder.svg"}
               alt="profile"
               className="h-8 w-8 rounded-full"
             />
